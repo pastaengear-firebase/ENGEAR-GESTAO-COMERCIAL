b4172a768de7dad0e9de66fe4e654dad8f1778ba
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-26 21:15:00 +0000

    Estou prestes a publicar a aplicação.... Favor analisar e revisar tudo,

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  allowedDevOrigins: ['https://6000-firebase-studio-1747639280840.cluster-etsqrqvqyvd4erxx7qq32imrjk.cloudworkstations.dev'],
 };
 
 export default nextConfig;
