aa78871671bd7060f21cc67d61686496ddf5df0c
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-27 20:37:32 +0000

    lembre que vc não pode usar da parte do servidor do Next.js.,

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ativa a exportação estática para compatibilidade com o plano Spark.
   output: 'export',
-
-  // A otimização de imagens do Next.js não é suportada no modo de exportação estática.
-  // Esta opção desativa a otimização para evitar erros.
   images: {
     unoptimized: true,
   },
