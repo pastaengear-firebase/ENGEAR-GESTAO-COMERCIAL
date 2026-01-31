e0c37c93fd07ffdeb3a197ec0a36c87e54da60f0
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-31 18:39:31 +0000

    O problema permanece, mas meu foco é publicar o projeto... Minha consult

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 
 // This configuration was provided by the user to connect to their Firebase project.
 export const firebaseConfig = {
-  apiKey: "AIzaSyC2_o9J8y7x6Z-f5G1h3k2l1j0m9n8b7v6",
+  apiKey: "AIzaSyB-...",
   authDomain: "engear---depto-comercial-v3.firebaseapp.com",
   projectId: "engear---depto-comercial-v3",
   storageBucket: "engear---depto-comercial-v3.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:a1b2c3d4e5f6a7b8c9d0e1",
-  measurementId: "G-ABC123DEF4"
+  messagingSenderId: "...",
+  appId: "1:...:web:...",
+  measurementId: "G-..."
 };
