eea75a9c13c884f405e996da47689bf2638c2b8a
Sergio Vasconcelos <pastaengear@gmail.com>
2026-01-31 18:47:18 +0000

    Não funciona, a mesma mensagem de API aparece. Anelise profundamente, va

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,35 @@
 // src/firebase/config.ts
 
-// This configuration was provided by the user to connect to their Firebase project.
+// IMPORTANTE: Por favor, preencha os valores marcados como "Cole o valor..." com os dados do seu projeto Firebase.
+// O erro 'auth/api-key-not-valid' indica que estes valores estão incorretos.
+
+// Para encontrar estes valores:
+// 1. Acesse o Firebase Console: https://console.firebase.google.com/
+// 2. Selecione seu projeto: 'engear---depto-comercial-v3'
+// 3. No menu à esquerda, clique no ícone de engrenagem (ao lado de 'Visão geral do projeto') e vá para "Configurações do projeto".
+// 4. Na aba "Geral", role para baixo até a seção "Seus apps".
+// 5. Selecione o seu aplicativo da Web (geralmente tem um ícone </ >).
+// 6. Você verá um objeto de configuração `firebaseConfig`. Copie os valores correspondentes para cá.
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-...",
+  // Cole o valor de 'apiKey' do seu projeto Firebase aqui.
+  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+
+  // O Auth Domain deve estar correto.
   authDomain: "engear---depto-comercial-v3.firebaseapp.com",
+
+  // O Project ID deve estar correto.
   projectId: "engear---depto-comercial-v3",
+
+  // O Storage Bucket deve estar correto.
   storageBucket: "engear---depto-comercial-v3.appspot.com",
-  messagingSenderId: "...",
-  appId: "1:...:web:...",
-  measurementId: "G-..."
+
+  // Cole o valor de 'messagingSenderId' do seu projeto Firebase aqui.
+  messagingSenderId: "000000000000",
+
+  // Cole o valor de 'appId' do seu projeto Firebase aqui.
+  appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx",
+
+  // Cole o valor de 'measurementId' do seu projeto Firebase aqui (opcional para Auth).
+  measurementId: "G-XXXXXXXXXX"
 };
