16b19a3de79a3bbe68ff3c47a1b9c7573dac744a
Sergio Vasconcelos <pastaengear@gmail.com>
2026-02-02 14:52:17 +0000

    Faça tudo com a maior responsabilidade e atenta em erros, emule e teste

--- contexts/app-provider.tsx
+++ contexts/app-provider.tsx
@@ -5,7 +5,7 @@ import { SalesProvider } from './sales-context';
 import { SettingsProvider } from './settings-context';
 import { QuotesProvider } from './quotes-context';
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { FirebaseClientProvider } from '@/firebase';
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
