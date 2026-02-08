625471cca48eefae06c901ce114cba67204d62a4
pastaengear-firebase <pastaengear@gmail.com>
2026-02-08 00:10:17 -0300

    Create layout.tsx file for RootLayout component

---
+++ src/app/layout.tsx
@@ -0,0 +1,1 @@
+'use client'; import React from 'react'; export default function RootLayout({ children, }: { children: React.ReactNode; }) { return ( <html lang="pt-BR"> <head> <meta charSet="utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1" /> <title>ENGEAR - Gestão Comercial</title> </head> <body> {children} </body> </html> ); }
