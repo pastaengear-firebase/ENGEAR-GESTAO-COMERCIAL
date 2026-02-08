f7f210dee4834567959e70fde9f4ffaca0828f12
pastaengear-firebase <pastaengear@gmail.com>
2026-02-08 00:05:35 -0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
